f1d60b24a300582d4d9054ef7fc5630d096e02dc
Nathan Lacerda <nathan.lacerda@escolarmanager.com.br>
2020-05-19 10:48:05 -0300

    Simplificação de código, alguns fixes de legibilidade, mais testes e mais limpeza de código.

--- TrabalhoCalculadora/Calculadora/CalculadoraTests.cs
+++ TrabalhoCalculadora/Calculadora/CalculadoraTests.cs
@@ -3,12 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Xunit;
 
-namespace Trabalho.Calculadora
+namespace Calculadora.Calculadora
 {
+    /*
+     * Nesta classe de testes implementei o MemberData e o ClassData para fins de aprendizado, porém acredito
+     * que esses são melhores para organizar testes maiores, já que o InlineData é mais direto e pode "poluir"
+     * o código, nos casos de muitos testes e muita data para que serão inseridos no teste para executá-lo.
+     * 
+     * Padrão nos métodos utilizado: Método a ser testado_Informação Sobre o Cenário do Teste_O resultado esperado.
+     * Exemplo: Soma_DoisInteiros_ResultaInteiroErrado - Este teste vai testar a função Add da Calculadora com
+     * dois números inteiros e retornar um inteiro errado, ou seja, se 2 + 2 é 7 o Assert deve ser NotEqual, já
+     * que isso é falso.
+     * 
+     * Aprendizados: Testes com xUnit, InlineData, MemberData, ClassData, IEnumerator, yield return, Padrão de nome dos métodos
+     * dos testes, legibilidade e simplificação de código.
+     */
+
     public class CalculadoraTests
     {
         [Fact]
-        public void Soma_DoisInteiros_ResultaInteiroErrado() => Assert.Equal(4, new Calculadora<int>().Add(2, 2));
+        public void Soma_DoisInteiros_ResultaInteiroErrado() => Assert.NotEqual(7, new Calculadora<int>().Add(2, 2));
+
+        [Fact]
+        public void Soma_DuasStrings_ResultaStringErrada() => Assert.NotEqual("ab cd", new Calculadora<string>().Add("ab", "cd"));
+
+        [Fact]
+        public void Soma_DoisDecimas_ResultaDecimalErrado() => Assert.NotEqual(4.1M, new Calculadora<decimal>().Add(2M, 2M));
 
         [Fact]
         public void Soma_DoisDouble_ResultadoDoubleErrado() => Assert.NotEqual(5, new Calculadora<int>().Add(2, 2));
@@ -19,7 +39,8 @@ namespace Trabalho.Calculadora
         [InlineData(-2, 2, 0)]
         [InlineData(int.MinValue, -1, int.MaxValue)]
         [InlineData(46851415, 46851415, 93702830)]
-        public void Soma_DoisInteiros_RetornaNumeroInteiro(int valor1, int valor2, int esperado) => Assert.Equal(esperado, new Calculadora<int>().Add(valor1, valor2));
+        public void Soma_DoisInteiros_RetornaNumeroInteiro(int valor1, int valor2, int esperado) => 
+            Assert.Equal(esperado, new Calculadora<int>().Add(valor1, valor2));
 
         [Theory]
         [InlineData("ab", "cd", "abcd")]
@@ -27,22 +48,27 @@ namespace Trabalho.Calculadora
         [InlineData(" ", " ", "  ")]
         [InlineData("Escolar ", "Manager", "Escolar Manager")]
         [InlineData("Nathan ", " Guedes", "Nathan  Guedes")]
-        public void Soma_DuasStrings_RetornaString(string valor1, string valor2, string esperado) => Assert.Equal(esperado, new Calculadora<string>().Add(valor1, valor2));
+        public void Soma_DuasStrings_RetornaString(string valor1, string valor2, string esperado) => 
+            Assert.Equal(esperado, new Calculadora<string>().Add(valor1, valor2));
 
         [Theory]
         [ClassData(typeof(CalculadoraTestsData))]
-        public void Soma_DoisDecimais_RetornaDecimal(decimal valor1, decimal valor2, decimal esperado) => Assert.Equal(esperado, new Calculadora<decimal>().Add(valor1, valor2), 1);
+        public void Soma_DoisDecimais_RetornaDecimal(decimal valor1, decimal valor2, decimal esperado) => 
+            Assert.Equal(esperado, new Calculadora<decimal>().Add(valor1, valor2), 1);
 
         [Theory]
         [MemberData(nameof(Data))]
-        public void Soma_DoisDoubles_RetornaDoubles(double valor1, double valor2, double esperado) => Assert.Equal(esperado, new Calculadora<double>().Add(valor1, valor2), 1);
+        public void Soma_DoisDoubles_RetornaDoubles(double valor1, double valor2, double esperado) => 
+            Assert.Equal(esperado, new Calculadora<double>().Add(valor1, valor2), 1);
 
         [Theory]
         [InlineData(true, true)]
         [InlineData(false, false)]
         [InlineData(true, false)]
-        public void Soma_DoisBools_RetornaFormatException(bool valor1, bool valor2) => Assert.Throws<FormatException>(() => new Calculadora<bool>().Add(valor1, valor2));
+        public void Soma_DoisBools_RetornaFormatException(bool valor1, bool valor2) => 
+            Assert.Throws<FormatException>(() => new Calculadora<bool>().Add(valor1, valor2));
 
+        /* Dados que serão inseridos no teste utilizando o MemberData. */
         public static IEnumerable<object[]> Data => new List<object[]>
         {
             new object[] { 1.33, 2.57, 3.9 },
@@ -51,6 +77,7 @@ namespace Trabalho.Calculadora
         };
     }
 
+    /* Mais dados que serão inseridos em um teste, porém aqui em classe diferente, já que utilizarei o ClassData. */
     public class CalculadoraTestsData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
--- TrabalhoCalculadora/Principal.cs
+++ TrabalhoCalculadora/Principal.cs
@@ -1,79 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Trabalho
+namespace Calculadora
 {
     public class Trabalho
     {
-        public static void Main(string[] args)
-        {
-            Calculadora<decimal> CalcTeste = new Calculadora<decimal>();
-            var v = CalcTeste.Add(79228162M, 1);
+        /* 
+         * Coloquei o método dessa forma apenas para fim de compilação, já que o objetivo desse projeto é o
+         * aprendizado de C#, OO e testes (aprendi delegate também por curiosidade e por sugestão do meu 
+         * supervisor).
+         */
 
-            var convertido = Convert.ToUInt32(CalcTeste);
-
-            Calculadora<double> calc = new Calculadora<double>();
-            var v1 = calc.Add(20.50, 12.40);
-
-            Calculadora<int> calc2 = new Calculadora<int>();
-            var v2 = calc2.Add(100, 200);
-
-            Calculadora<string> calc3 = new Calculadora<string>();
-            var v3 = calc3.Add("ab", "cd");
-
-            Pessoa p = new Pessoa();
-            Pessoa p1 = new Pessoa(1, 10, "Nathan");
-            Pessoa p2 = p1;
-
-            Pessoa p3 = new Pessoa(1, 10, "João");
-            Pessoa p4 = new Pessoa(1, 10, "João");
-
-
-
-            /* Downcast & Upcast */
-
-            Pessoa Pessoa = new Funcionario(); //Upcast
-            /* 
-             * Downcast & Conversão explícita - Aqui é necessário fazer o cast do funcionário, já que a Pessoa não herda de Funcionário e sim ao contrário.
-             * Nesse caso Pessoa pegou o que Funcionario herda da classe Pessoa, porém nenhum método de funcionário pode instanciar Pessoa.
-             */
-            Funcionario f = (Funcionario)Pessoa; //Downcast
-            Funcionario f2 = new Funcionario();
-            /*
-             * Upcast & Conversão implícita - Aqui já não precisamos do cast do funcionário, já que dentro de Funcionário temos o objeto Pessoa e podemos, sem
-             * a utilização de cast, instanciar diretamente, porém não teremos acesso aos atributos de Funcionário, apenas os de Pessoa.
-             * PS: Também é aceito uma conversão explícita, porém não é necessário.
-             */
-            Pessoa Pessoa2 = f2;
-            Pessoa Pessoa3 = (Pessoa)f2;
-
-            Pessoa.Cep = 1;
-            var Name = Pessoa.Nome;
-
-
-            /* Exemplos de usos de List<T> */
-            List<Funcionario> funcionarios = new List<Funcionario>();
-            funcionarios.Add(f);
-            funcionarios.Add(f2);
-            funcionarios.Add((Funcionario)Pessoa);
-
-            //lista.Add(Pessoa2); // Aqui não é possível adicionar uma Pessoa, pois ela não tem nenhuma herança de Funcionário.
-
-            List<Pessoa> pessoas = new List<Pessoa>();
-            pessoas.Add(Pessoa2);
-            pessoas.Add(f);
-
-
-
-            List<Pessoa> lista2 = new List<Pessoa>();
-            lista2.Add(f); // Aqui já podemos adicionar um Funcionário, pois este herda uma Pessoa.
-
-            /* Fazer uma leitura de toda a lista usando lambda, código limpo e fácil de ler */
-            funcionarios.ForEach(x => x.ToString());
-
-            /* Criando lista a partir de array pronta */
-            Pessoa[] array = new Pessoa[] { Pessoa, f2 };
-            //List<Pessoa> pessoas = new List<Pessoa>(array);
-        }
+        public static void Main(string[] args) { }
     }
 }
