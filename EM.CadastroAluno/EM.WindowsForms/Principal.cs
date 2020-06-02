acf5fd550ab4b012e28d1e9cfcb3d96ee70bd73d
Nathan Lacerda <nathan.lacerda@escolarmanager.com.br>
2020-06-02 14:22:19 -0300

    Retirada de herança desnecessária.

--- EM.CadastroAluno/EM.WindowsForms/Principal.cs
+++ EM.CadastroAluno/EM.WindowsForms/Principal.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace EM.WindowsForms
 {
-    public class Principal : Form
+    public class Principal
     {
         /// <summary>
         ///  The main entry point for the application.
